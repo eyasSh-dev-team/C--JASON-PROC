b6a4258204a526f52e4a90d0f13e3890e82bab19
EyasSh <75741927+EyasSh@users.noreply.github.com>
2020-12-09 19:00:10 +0200

    Add files via upload

---
+++ CS2JASON/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2JASON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region comment
+            /*Console.WriteLine("hello cruel world");*/
+            #endregion comment
+            int numOfchunks = 250; //indicates the num of chinksu want to render
+            string blockHash = "#H53F-5E9A-15D";//indicates the number of a new matrial block
+            const string GIVE= "give"; 
+            const string HELP = "help";
+            const string TELEPORT = "tp";
+            const string KILL = "kill";
+            const string WEATHER = "weather";
+            const string GAMEMODE = "gamemode";
+            const string TIME = "time";
+            const string DIFFICULTY = "difficulty";
+            const string GAMERULE = "gamerule";
+            const string SUMMON ="summon"; //summon mobs 
+            const string ATLANTIS = "atlantis"; //raises world water level 
+            const string RIDE = "ride"; //turns a creature you're facing into a mount 
+            const string INSTANTMINE ="instantmine";//one click mine with any tool 
+            const string FREEZE ="freeze"; //freezes  mobs or people
+            const string FALLDAMAGE = "falldamage"; //fall damage on or off
+            const string FIREDAMAGE = "firedamge"; //fire damage on or off
+            const string WATERDAMAGE = "waterdamage"; //water damage on or off
+            const string SUPERHEAT = "superheat"; //smelts something 
+            const string INSTANTPLANT = "instantplant"; //instantly grows planted seeds
+            const string DROPSTORE = "drompstore"; //Stores all inventory items into a chest, which spawns nearby.
+            const string ITEMDAMAGE = "itemdamage"; //Weapons no longer receive damage or degrade.
+            const string DUPLICATE = "duplicate";
+            const string SEED = "seed";//gets world seed
+
+
+
+        }
+    }
+
+}
